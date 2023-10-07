1a5e191242f99b2a15c7eba78cb977c47bb8a973
Olena <yelenastankevych@gmail.com>
2023-10-08 02:57:17 +0300

    Initial commit

---
+++ src/test/java/mate/academy/BookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
