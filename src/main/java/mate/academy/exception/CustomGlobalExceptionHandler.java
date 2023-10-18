5691820b35006fe157ae01e4e5d979994c1f6dfd
Olena <yelenastankevych@gmail.com>
2023-10-18 11:30:57 +0300

    refactored CustomGlobalExceptionHandler

--- src/main/java/mate/academy/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/exception/CustomGlobalExceptionHandler.java
@@ -4,28 +4,22 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import mate.academy.exception.errors.BookApiError;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomGlobalExceptionHandler {
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex,
-            HttpHeaders headers,
-            HttpStatusCode status,
-            WebRequest request) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> invalidArgument(MethodArgumentNotValidException ex) {
         return buildResponseEntity(new BookApiError(BAD_REQUEST,
                 LocalDateTime.now(), getErrorsMessage(ex.getBindingResult())));
     }
@@ -42,9 +36,10 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         return new ResponseEntity<>(bookApiError, bookApiError.status());
     }
 
-    private List<String> getErrorsMessage(BindingResult bindingResult) {
+    private Map<String, String> getErrorsMessage(BindingResult bindingResult) {
         return bindingResult.getAllErrors().stream()
-                .map(e -> ((FieldError) e).getField() + ": " + e.getDefaultMessage())
-                .toList();
+                .collect(Collectors.groupingBy(e -> ((FieldError)e).getField(),
+                        Collectors.mapping(DefaultMessageSourceResolvable::getDefaultMessage,
+                                Collectors.joining())));
     }
 }
