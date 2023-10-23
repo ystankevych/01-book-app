9056d3a5e5d4b28beae7db686e3f12e8e5b1eb7b
ystankevych <105653924+ystankevych@users.noreply.github.com>
2023-10-23 11:55:12 +0300

    Implemented pagination, added springdoc-openapi-starter-webmvc-ui dep… (#5)
    * Implemented pagination, added springdoc-openapi-starter-webmvc-ui dependency
    
    * Added liquibase plugin (сщі I lost it when I was trying to resolve the conflict)
    
    * added validation for the path variable in the controller
    
    * added validation for the path variable in the controller
    
    * added fixes

--- src/main/java/mate/academy/service/BookService.java
+++ src/main/java/mate/academy/service/BookService.java
@@ -4,17 +4,19 @@ import java.util.List;
 import mate.academy.dto.BookDto;
 import mate.academy.dto.BookSearchParametersDto;
 import mate.academy.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto book);
 
     BookDto updateBook(Long id, CreateBookRequestDto book);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
     void deleteById(Long id);
 
-    List<BookDto> findByParameters(BookSearchParametersDto bookSearchParametersDto);
+    List<BookDto> findByParameters(BookSearchParametersDto bookSearchParametersDto,
+                                   Pageable pageable);
 }
