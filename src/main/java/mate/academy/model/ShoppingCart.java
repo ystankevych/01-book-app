4cc3d5d5ff899331b562dd5b1dfb8e6ed5201364
Olena <yelenastankevych@gmail.com>
2023-10-31 13:22:02 +0200

    added an empty line

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -2,11 +2,16 @@ package mate.academy.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE shopping_carts SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
@@ -16,5 +21,6 @@ public class ShoppingCart {
     @JoinColumn(name = "id")
     private User user;
     @OneToMany(mappedBy = "shoppingCart")
-    private Set<CartItem> cartItems;
+    private Set<CartItem> cartItems = new HashSet<>();
+    private boolean isDeleted;
 }
