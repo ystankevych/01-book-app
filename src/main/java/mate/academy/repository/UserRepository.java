1ff0392d253b6d7f9a616c8ee71ce76c42d9d8d1
Olena <yelenastankevych@gmail.com>
2023-10-31 13:28:30 +0200

    added fixes after mentor review

--- src/main/java/mate/academy/repository/UserRepository.java
+++ src/main/java/mate/academy/repository/UserRepository.java
@@ -10,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     @EntityGraph(attributePaths = "roles")
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
--- src/main/java/mate/academy/validation/email/EmailValidator.java
+++ src/main/java/mate/academy/validation/email/EmailValidator.java
@@ -5,12 +5,13 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.util.regex.Pattern;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
-    private static final String EMAIL_PATTERN =
-            "^(?=.{1,64}@)[\\w-]+(.[\\w-]+)*@[^-][A-Za-z0-9-]+(.[A-Za-z0-9-]+)*(.[A-Za-z]{2,})$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^(?=.{1,64}@)[\\w-]+(.[\\w-]+)*@[^-][A-Za-z0-9-]+(.[A-Za-z0-9-]+)*(.[A-Za-z]{2,})$"
+    );
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
-        return email != null && Pattern.compile(EMAIL_PATTERN)
+        return email != null && EMAIL_PATTERN
                 .matcher(email)
                 .matches();
     }
