4235d825c41f685cdf6c9400907567de0644a783
Olena <yelenastankevych@gmail.com>
2023-11-15 21:05:10 +0200

    added Dockerfile, docker-compose.yml, refactored application.properties

--- src/main/java/mate/academy/repository/CartItemRepository.java
+++ src/main/java/mate/academy/repository/CartItemRepository.java
@@ -8,6 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     Optional<CartItem> findByIdAndShoppingCartId(Long itemId, Long cartId);
-
-    void deleteAllByShoppingCart_Id(Long cartId);
 }
