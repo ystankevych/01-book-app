eaa83ccd580376f21d007f0b396efa2416d7e3df
Olena <yelenastankevych@gmail.com>
2023-11-29 21:18:46 +0200

    added tests for shoppingcart controller

--- src/main/java/mate/academy/dto/shoppingcart/ShoppingCartDto.java
+++ src/main/java/mate/academy/dto/shoppingcart/ShoppingCartDto.java
@@ -1,12 +1,10 @@
 package mate.academy.dto.shoppingcart;
 
 import java.util.List;
-import java.util.Set;
 import mate.academy.dto.cartitem.CartItemDto;
 
 public record ShoppingCartDto(
         Long id,
         Long userId,
         List<CartItemDto> cartItems) {
-
 }
--- src/test/java/mate/academy/controller/ShoppingCartControllerTest.java
+++ src/test/java/mate/academy/controller/ShoppingCartControllerTest.java
@@ -1,20 +1,26 @@
 package mate.academy.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import java.util.stream.Stream;
 import mate.academy.dto.cartitem.CartItemDto;
 import mate.academy.dto.cartitem.CartItemRequestDto;
+import mate.academy.dto.cartitem.UpdateCartItemDto;
 import mate.academy.dto.shoppingcart.ShoppingCartDto;
 import mate.academy.exception.BookApiErrorResponse;
 import mate.academy.exception.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -22,18 +28,13 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlMergeMode;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.util.Map;
-import java.util.stream.Stream;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "classpath:database/create/add-default-user-cart.sql")
@@ -111,7 +112,8 @@ class ShoppingCartControllerTest {
                 .andExpect(result -> assertTrue(result.getResolvedException()
                         instanceof EntityNotFoundException))
                 .andExpect(result -> assertEquals("Book with id: %d is not found"
-                        .formatted(requestDto.bookId()), result.getResolvedException().getMessage()));
+                        .formatted(requestDto.bookId()),
+                        result.getResolvedException().getMessage()));
     }
 
     @ParameterizedTest
@@ -150,6 +152,90 @@ class ShoppingCartControllerTest {
     @Sql(scripts = {ADD_BOOK, ADD_CART_ITEM})
     @Sql(scripts = {CLEAR_CART, DELETE_BOOK, DELETE_USER_CART},
             executionPhase = AFTER_TEST_METHOD)
-    void updateBookQuantity_ExistingCartItem_Ok() {
+    void updateBookQuantity_ExistingCartItem_Ok() throws Exception {
+        UpdateCartItemDto request = new UpdateCartItemDto(5);
+
+        MvcResult result = mockMvc.perform(put("/cart/cart-items/1")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        CartItemDto expected = new CartItemDto(1L, 1L, "Rich Dad Poor Dad", 5);
+        ShoppingCartDto actual = objectMapper.readValue(result.getResponse().getContentAsString(),
+                ShoppingCartDto.class);
+
+        assertAll("actual",
+                () -> assertEquals(1, actual.id()),
+                () -> assertEquals(1, actual.userId()),
+                () -> assertEquals(1, actual.cartItems().size()),
+                () -> assertTrue(actual.cartItems().contains(expected)));
+    }
+
+    @Test
+    @WithUserDetails(value = "user@ukr.net")
+    @Sql(scripts = DELETE_USER_CART, executionPhase = AFTER_TEST_METHOD)
+    void updateBookQuantity_NonExistentCartItem_NotOk() throws Exception {
+        UpdateCartItemDto cartItemDto = new UpdateCartItemDto(5);
+
+        mockMvc.perform(put("/cart/cart-items/10")
+                        .content(objectMapper.writeValueAsString(cartItemDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> assertTrue(result.getResolvedException()
+                        instanceof EntityNotFoundException))
+                .andExpect(result -> assertEquals("No cart item with id: %d for user: %d"
+                        .formatted(10, 1), result.getResolvedException().getMessage()));
+    }
+
+    @ParameterizedTest
+    @Sql(scripts = DELETE_USER_CART, executionPhase = AFTER_TEST_METHOD)
+    @MethodSource("invalidUpdateCartItemDto")
+    @WithUserDetails(value = "user@ukr.net")
+    void updateBookQuantity_InvalidRequestDto_NotOk(UpdateCartItemDto requestDto,
+                                                    String fieldName,
+                                                    String errorMessage) throws Exception {
+        MvcResult result = mockMvc.perform(put("/cart/cart-items/1")
+                        .content(objectMapper.writeValueAsString(requestDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        BookApiErrorResponse errorResponse = objectMapper.readValue(result.getResponse()
+                .getContentAsString(), BookApiErrorResponse.class);
+        Map<String, String> erroreMap = (Map<String, String>) errorResponse.errorMessages();
+        assertEquals(erroreMap.get(fieldName), errorMessage);
+    }
+
+    private static Stream<Arguments> invalidUpdateCartItemDto() {
+        return Stream.of(
+                Arguments.of(new UpdateCartItemDto(0),
+                        "quantity", "Quantity must be positive"),
+                Arguments.of(new UpdateCartItemDto(-1),
+                        "quantity", "Quantity must be positive")
+        );
+    }
+
+    @Test
+    @WithUserDetails(value = "user@ukr.net")
+    @Sql(scripts = {ADD_BOOK, ADD_CART_ITEM})
+    @Sql(scripts = {CLEAR_CART, DELETE_BOOK, DELETE_USER_CART},
+            executionPhase = AFTER_TEST_METHOD)
+    void deleteBookFromCart_ExistingCartItemId_Ok() throws Exception {
+        mockMvc.perform(delete("/cart/cart-items/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andReturn();
+    }
+
+    @Test
+    @WithUserDetails(value = "user@ukr.net")
+    @Sql(scripts = DELETE_USER_CART, executionPhase = AFTER_TEST_METHOD)
+    void deleteBookFromCart_NonExistentCartItemId_ThrowException() throws Exception {
+        mockMvc.perform(delete("/cart/cart-items/10"))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> assertTrue(result.getResolvedException()
+                        instanceof EntityNotFoundException))
+                .andExpect(result -> assertEquals("No such cartItem with id %d in shopping cart"
+                        .formatted(10), result.getResolvedException().getMessage()));
     }
 }
