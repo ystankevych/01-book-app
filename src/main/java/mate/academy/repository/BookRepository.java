de5f9b4311b6f0b373e27916e245b5189e42caf3
Olena <yelenastankevych@gmail.com>
2023-11-29 11:36:52 +0200

    made some changes

--- src/main/java/mate/academy/repository/BookRepository.java
+++ src/main/java/mate/academy/repository/BookRepository.java
@@ -1,7 +1,12 @@
 package mate.academy.repository;
 
 import java.util.List;
+import java.util.Optional;
 import mate.academy.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -9,4 +14,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     List<Book> findByCategories_Id(Long categoryId);
+
+    @EntityGraph(attributePaths = "categories")
+    Page<Book> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = "categories")
+    Page<Book> findAll(Specification<Book> specification, Pageable pageable);
+
+    @EntityGraph(attributePaths = "categories")
+    Optional<Book> findById(Long id);
 }
