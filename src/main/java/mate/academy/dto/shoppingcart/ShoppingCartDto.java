39f0aa721bff9de6bd8b9f709ede708c1c9dd778
Olena <yelenastankevych@gmail.com>
2023-11-29 17:32:39 +0200

    made some changes

--- src/main/java/mate/academy/dto/shoppingcart/ShoppingCartDto.java
+++ src/main/java/mate/academy/dto/shoppingcart/ShoppingCartDto.java
@@ -1,11 +1,12 @@
 package mate.academy.dto.shoppingcart;
 
+import java.util.List;
 import java.util.Set;
 import mate.academy.dto.cartitem.CartItemDto;
 
 public record ShoppingCartDto(
         Long id,
         Long userId,
-        Set<CartItemDto> cartItems) {
+        List<CartItemDto> cartItems) {
 
 }
---
+++ src/test/resources/database/create/add-one-book.sql
@@ -0,0 +1,2 @@
+INSERT INTO books (id, title, author, isbn, price)
+VALUES (1, 'Rich Dad Poor Dad', 'Kiyosaki', '1111111111', 14.99);
